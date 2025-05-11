313852ff7c5dc460ceaca01791331f16cf606081
Jose Terrazas <jozaguts@gmail.com>
2025-05-11 02:01:05 -0600

    feat(localization): wrap home route in localization middleware group

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
+Route::group(['prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function() {
+    Route::get('/', function () {
+        return view('home');
+    });
 });
+
