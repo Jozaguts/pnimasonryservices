71af56113732b6512f17215eeafa1cfc178fabfa
Jose Terrazas <jozaguts@gmail.com>
2025-05-12 01:19:14 -0600

    feat(services): add ServiceController for handling service detail display

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ServiceController extends Controller
+{
+    public function show(string $service)
+    {
+        $data = trans("services.$service");
+
+        if (!is_array($data)) {
+            abort(404);
+        }
+
+        return view('services.show', [
+            'translatedName' => $data['title'],
+            'description' => $data['description'],
+        ]);
+    }
+}
